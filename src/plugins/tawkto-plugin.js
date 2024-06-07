c9c59fa7efe14f8eb3bb43c79e2208d39857c860
Thành Nam Nguyễn <namnguyenthanh.work@gmail.com>
2024-06-07 09:54:33 +0700

    first commit

---
+++ src/plugins/tawkto-plugin.js
@@ -0,0 +1,28 @@
+// /**
+//  * Copyright @ by Code Lyoko Team. All rights reserved.
+//  * Author: Thành Nam Nguyễn
+//  */
+
+module.exports = function () {
+  return {
+    injectHtmlTags() {
+      return {
+        postBodyTags: [
+          {
+            tagName: 'script',
+            innerHTML: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();`,
+          },
+        ],
+      };
+    },
+  };
+};
